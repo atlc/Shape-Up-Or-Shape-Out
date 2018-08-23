8a6a1aa97b5054b0e606a2a985aed4c3cf430259
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-23 11:09:07 -0500

    Structured all of the required HTML and added Bootstrap locally since I will be away from the internet and most civilization for the weekend.

---
+++ app.js
@@ -0,0 +1,29 @@
+class Shape {
+    constructor(name, width, height) {
+
+    }
+}
+
+class Rectangle extends Shape {
+    constructor() {
+
+    }
+}
+
+class Square extends Shape {
+    constructor() {
+        
+    }
+}
+
+class Circle extends Shape {
+    constructor() {
+        
+    }
+}
+
+class Triangle extends Shape {
+    constructor() {
+        
+    }
+}
