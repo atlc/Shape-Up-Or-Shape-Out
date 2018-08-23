3a51cf7d7559fca35a871d4f897161acdbb651ac
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-23 12:13:35 -0500

    Quick start at setting up classe

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 class Shape {
     constructor(name, width, height) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
 
+    area() {
+        return (this.width * this.height);
+    }
+
+    perimeter() {
+        return ((2 * this.width) + (2 * this.height));
     }
 }
 
 class Rectangle extends Shape {
     constructor() {
-
+        super();
     }
 }
 
 class Square extends Shape {
     constructor() {
+        super();
         
     }
 }
 
 class Circle extends Shape {
-    constructor() {
-        
+    constructor(radius) {
+        super();
+        this.radius = radius;
     }
 }
 
 class Triangle extends Shape {
     constructor() {
-        
+        super();
     }
 }
